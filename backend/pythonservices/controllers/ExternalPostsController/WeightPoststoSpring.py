7291176e01d769bd6eb01ca8450257355e2a5592
BhojGC <gcbhoj@gmail.com>
2026-02-13 23:37:59 -0500

    Added necessary comments

--- backend/pythonservices/controllers/ExternalPostsController/WeightPoststoSpring.py
+++ backend/pythonservices/controllers/ExternalPostsController/WeightPoststoSpring.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, request, jsonify
 from services.RasperrryPiServices.postWeightToSpring import post_weight
 from services.ExternalServices.postToServer import send_post_request_to_spring
+
+
+
+
+#spring boot application path 
 springPath = "http://localhost:500/api/v1/itemWeight"
 
-#BlueprintDefinition
 
+
+#BlueprintDefinition
 post_to_spring = Blueprint('post_to_spring',__name__)
 
+
+
+#The following block of code is called by spring boot application when a shopper is adding loose items to the cart.
 @post_to_spring.route('/api/py/weight', methods=['POST'])
 def post_weight_to_spring():
     try:
--- backend/pythonservices/models/PostWeightDTO.py
+++ backend/pythonservices/models/PostWeightDTO.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+# Data transfer model to post the weight of the loose items to spring
 
 @dataclass
 class PostWeightDTO:
